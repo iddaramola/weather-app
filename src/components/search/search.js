b65eb343695a5cec521d86386b3eb143aacdf7b5
Idowu Daramola <idowu.daramola@gmail.com>
2023-08-27 14:25:45 +0100

    Finish Search Component

---
+++ src/components/search/search.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+import { AsyncPaginate } from "react-select-async-paginate";
+
+import { GEO_API_URL, geoApiOptions } from "../../geodbcities";
+
+
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptionsFromGeoDB = (inputValue) => {
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+            geoApiOptions
+        )
+            .then((response) => response.json())
+            .then((response) => {
+                //console.log(response);
+                console.log(response.data);      
+                //once the response comes back,we then use the response to get the shape of data AsyncPaginate needs, which is an object with 'options' key that maps to array of objects with keys, 'value' and 'label'
+                return {
+                    options: response.data.map((city)=> {
+                        
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch((err) => console.error(err));
+    };
+
+    
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={800}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptionsFromGeoDB}
+    />
+  );
+};
+
+export default Search;
---
+++ src/geodbcities.js
@@ -0,0 +1,11 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b5da2b7b03msha3640dc80877856p1f905ajsn1081e11ce9bf',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
